e02a150d5ae578221a8743f42cab79f780d68851
Mike Petrus <Petrus@ucsb.edu>
2025-06-27 13:06:42 -0700

    vec3 setup

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,32 @@
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    // Image
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+
+    std::cout << "P3\n" << image_width <<  ' ' << image_height << "\n255\n";
+
+    for (int i = 0; i < image_height; i++)
+    {
+        std::clog << "\rScnalines remaining: " << (image_height - i) << ' ' << std::flush;
+
+        for (int j = 0; j < image_width; j++)
+        {
+            double r = double(j) / (image_width - 1);
+            double g = double(i) / (image_height - 1);
+            double b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    std::clog << "\rDone.       \n";
     return 0;
 }
