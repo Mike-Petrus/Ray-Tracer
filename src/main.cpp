9802a654e214200448693732d3d9b01c020882b5
Mike Petrus <Petrus@ucsb.edu>
2025-06-28 13:08:51 -0700

    blue gradient

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,44 @@
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
 #include <iostream>
 
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    double a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0 - a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+}
+
 int main()
 {
     // Image
-    int image_width = 256;
-    int image_height = 256;
+    
+    double aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+
+    // Calculate the image height, and esnure that it's at least 1
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Camera
+
+    double focal_length = 1.0;
+    double viewport_height = 2.0;
+    double viewport_width = viewport_height * (double(image_width)/image_height);
+    point3 camera_center = point3(0, 0, 0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges
+    vec3 viewport_u = vec3(viewport_width, 0, 0);
+    vec3 viewport_v = vec3(0, -viewport_height, 0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel
+    vec3 pixel_delta_u = viewport_u / image_width;
+    vec3 pixel_delta_v = viewport_v / image_height;
+
+    // Calculate the location of the upper left pixel
+    point3 viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    point3 pixel_00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // Render
 
@@ -19,7 +50,11 @@ int main()
 
         for (int j = 0; j < image_width; j++)
         {
-            auto pixel_color = color(double(j)/(image_width - 1), double(i)/(image_height - 1), 0);
+            point3 pixel_center = pixel_00_loc + (j * pixel_delta_u) + (i * pixel_delta_v);
+            vec3 ray_direction = pixel_center - camera_center;
+            ray r = ray(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
