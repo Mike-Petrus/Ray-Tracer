bdb435829a7f1ba485080b4000a722eb1c76b8b1
Mike Petrus <Petrus@ucsb.edu>
2025-06-30 01:44:51 -0700

    Antialiasing + random utility funcs

--- src/camera.h
+++ src/camera.h
@@ -7,6 +7,7 @@ class camera {
     public:
         double aspect_ratio = 1.0;  // Ratio of image width over height
         int image_width = 100;      // Rendered image width in pixels
+        int samples_per_pixel = 10; // Count of random samples for each pixel
 
         void render(const hittable& world) {
             initialize();
@@ -19,12 +20,13 @@ class camera {
         
                 for (int j = 0; j < image_width; j++)
                 {
-                    point3 pixel_center = pixel_00_loc + (j * pixel_delta_u) + (i * pixel_delta_v);
-                    vec3 ray_direction = pixel_center - center;
-                    ray r(center, ray_direction);
-        
-                    color pixel_color = ray_color(r, world);
-                    write_color(std::cout, pixel_color);
+                    color pixel_color(0, 0, 0);
+
+                    for (int sample = 0; sample < samples_per_pixel; sample++) {
+                        ray r = get_ray(i, j);
+                        pixel_color += ray_color(r, world);
+                    }
+                    write_color(std::cout, pixel_samples_scale * pixel_color);
                 }
             }
         
@@ -32,16 +34,19 @@ class camera {
         }
 
     private:
-        int image_height;       // Rendered image height
-        point3 center;          // Camera center
-        point3 pixel_00_loc;    // Location of pixel 0,0 - top-left
-        vec3 pixel_delta_u;     // Offset to pixel to the right
-        vec3 pixel_delta_v;     // Offset to pixel below
+        int image_height;           // Rendered image height
+        double pixel_samples_scale; // Color scale factor for a sum of pixel samples
+        point3 center;              // Camera center
+        point3 pixel_00_loc;        // Location of pixel 0,0 - top-left
+        vec3 pixel_delta_u;         // Offset to pixel to the right
+        vec3 pixel_delta_v;         // Offset to pixel below
 
         void initialize() {
             // Calculate the image height, and esnure that it's at least 1
             image_height = int(image_width / aspect_ratio);
             image_height = (image_height < 1) ? 1 : image_height;
+
+            pixel_samples_scale = 1.0 / samples_per_pixel;
 
             center = point3(0, 0, 0);
 
@@ -62,6 +67,24 @@ class camera {
             // Calculate the location of the upper left pixel
             point3 viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
             pixel_00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+        }
+
+        ray get_ray(int i, int j) const {
+            // Construct a camer ray originating from the origin and directed at a randomly sampled
+            // point around the pixel i, j
+
+            vec3 offset = sample_square();
+            vec3 pixel_sample = pixel_00_loc + ((j + offset.x()) * pixel_delta_u) + ((i + offset.y()) * pixel_delta_v);
+
+            vec3 ray_origin = center;
+            vec3 ray_direction = pixel_sample - ray_origin;
+
+            return ray(ray_origin, ray_direction);
+        }
+
+        vec3 sample_square() const { 
+            // Returns the vector to a random point in the [-.5, -.5] to [+.5, +.5] unit square
+            return vec3(random_double() - 0.5, random_double() - 0.5, 0);
         }
 
         color ray_color(const ray& r, const hittable& world) const {
--- src/interval.h
+++ src/interval.h
@@ -11,12 +11,21 @@ class interval {
         interval(double min, double max) : min(min), max(max) {}
 
         double size() const { return max - min; }
+
         bool contains(double x) const {
             return min <= x && max >= x;
         }
+
         bool surrounds(double x) const {
             return min < x && max > x;
         }
+
+        double clamp(double x) const { 
+            if (x < min) return min;
+            if (x > max) return max;
+            return x;
+        }
+
 
         static const interval empty, universe;
 };
