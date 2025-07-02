bab0a5bd8f07e79b62d84084124e63b55c6c99a9
Mike Petrus <Petrus@ucsb.edu>
2025-07-02 02:08:09 -0700

    vector reflection, metal mat, scene update

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "camera.h"
 #include "hittable.h"
 #include "hittable_list.h"
+#include "material.h"
 #include "sphere.h"
 
 int main()
@@ -9,8 +10,15 @@ int main()
     // World
     hittable_list world;
 
-    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+    shared_ptr material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    shared_ptr material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    shared_ptr material_left = make_shared<metal>(color(0.8, 0.8, 0.8));
+    shared_ptr material_right = make_shared<metal>(color(0.8, 0.6, 0.2));
+
+    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100, material_ground));
+    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     // Camera
     camera cam;
--- src/material.h
+++ src/material.h
@@ -21,8 +21,23 @@ class lambertian : public material {
 
             // Catch degenerate scatter direction e.g. scatter is opposite normal
             if (scatter_direction.near_zero()) { scatter_direction = rec.normal; }
-            
+
             scattered = ray(rec.p, scatter_direction);
+            attenuation = albedo;
+            return true;
+        }
+
+    private:
+        color albedo;
+};
+
+class metal : public material {
+    public:
+        metal(const color& albedo) : albedo(albedo) {}
+
+        bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+            vec3 reflected = reflect(r_in.direction(), rec.normal);
+            scattered = ray(rec.p, reflected);
             attenuation = albedo;
             return true;
         }
