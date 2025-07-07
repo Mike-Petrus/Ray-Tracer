98eed047c6219a340be75d827198009d913494c2
Mike Petrus <Petrus@ucsb.edu>
2025-07-07 14:43:48 -0700

    metal fuzz logic

--- src/material.h
+++ src/material.h
@@ -33,17 +33,23 @@ class lambertian : public material {
 
 class metal : public material {
     public:
-        metal(const color& albedo) : albedo(albedo) {}
+        metal(const color& albedo, double fuzz) : albedo(albedo), fuzz(fuzz < 1 ? fuzz : 1) {}
 
         bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
             vec3 reflected = reflect(r_in.direction(), rec.normal);
+
+            // Fuzz logic, scatters ray in random direction scaled by fuzz
+            reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
             scattered = ray(rec.p, reflected);
             attenuation = albedo;
-            return true;
+
+            // Discards ray if scattered ray is below surface (scatter (dot) normal < 0)
+            return (dot(scattered.direction(), rec.normal) > 0);
         }
 
     private:
         color albedo;
+        double fuzz;
 };
 
 #endif
