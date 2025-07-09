08dc2d993aa1cffe845f3242222634b84f2354e7
Mike Petrus <Petrus@ucsb.edu>
2025-07-09 00:36:18 -0700

    Schlick approximation + glass sphere

--- src/material.h
+++ src/material.h
@@ -70,7 +70,13 @@ class dielectric : public material {
             bool cannot_refract = ri * sin_theta > 1.0;
             vec3 direction;
 
-            if (cannot_refract) { direction = reflect(unit_direction, rec.normal); }
+            // Reflectance > random_double() simulates the Fresnel effect
+            // As the angle of indcidence increases (grazing rays), reflectance increases
+            // and less light refracts
+            // this is a Monte Carlo decision: if reflectance() is HIGH, it's more
+            // likely to reflect. Low is more likely to refract. This simulates real materials
+            if (cannot_refract || reflectance(cos_theta, ri) > random_double()) { 
+                direction = reflect(unit_direction, rec.normal); }
             else { direction = refract(unit_direction, rec.normal, ri); }
 
             scattered = ray(rec.p, direction);
@@ -81,6 +87,13 @@ class dielectric : public material {
         // Refractive index in vacuum/air or the ratio of the material's refractive index
         // over the refractive index of the enclosing material
         double refraction_index;
+
+        static double reflectance(double cos_theta, double refraction_index) {
+            // Use Schlick approximation for reflectance
+            double r0 = (1 - refraction_index) / (1 + refraction_index);
+            r0 = r0 * r0;
+            return r0 + (1 - r0) * std::pow((1 - cos_theta), 5);
+        }
 };
 
 #endif
